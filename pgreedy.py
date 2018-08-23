0847f1a963c4d44488ae7cbf300a800aad991db0
Celien <c_bosm01@uni-muenster.de>
2018-08-23 15:22:48 +0200

    updated implementation of residual and power function, fixed the bug in notselected

--- pgreedy.py
+++ pgreedy.py
@@ -41,75 +41,88 @@ def train(discrete_omega, kernel, f, max_iterations, p_tolerance, norm_squarred=
 	# an array of the coefficients wrt the Newton basis
 	coeff = np.zeros((max_iterations, q))
 	# the residual evaluated on discrete_omega at each iteration
-	residual_eval = np.copy(f)
+	# shape (max_iterations, domega_length, q)
+	# axis 0 = iteration, axis 1 = data value, axis 2 = component
+	residual_eval = np.zeros((max_iterations, domega_length, q))
 	# the power function evaluated on discrete_omega at each iteration
-	power_eval = A[0,0] * np.ones(domega_length, dtype=np.longdouble)
+	# axis 0 = data value, axis 1 = iteration
+	power_eval = np.empty((domega_length, max_iterations))
+	power_eval.fill(np.nan)
 	# the basis transition matrix
 	change_of_basis = np.zeros((max_iterations, max_iterations))
-	# an array storing the maximum power function at each iteration
-	pmax = np.zeros(max_iterations)
-	# an array storing the max residual at each iteration
-	rmax = np.zeros(max_iterations)
 	# number of iterations
 	num_iterations = max_iterations
 	# an array containing the quadrature of f-surrogate at each iteration
 	residual_quad = np.array([])
 	# an array storing the interpolation error in rkhs norm at each iteration
 	rkhs_error = np.zeros(max_iterations)
-	v = np.zeros(max_iterations)
 
 	for k in range(0, max_iterations):
 		# point selection for this iteration
-		i = np.argmax(power_eval[notselected])
-		selected.append(notselected[i])
-		pmax[k] = np.max(power_eval[notselected])
+		if k > 0:
+			selection_index = np.argmax(power_eval[notselected,k-1])
+		else:
+			selection_index = np.argmax(A.diagonal())
+		selected.append(notselected[selection_index])
 
 		# computing the kth basis function
 		if k > 0:
 			a = basis_eval[notselected, 0:k].T
 			b = basis_eval[selected[k], 0:k]
 			basis_eval[notselected, k] = A[notselected, selected[k]].ravel() - b @ a
+			basis_eval[notselected, k] /= power_eval[selected[k], k-1]
 		else:
 			basis_eval[notselected, k] = A[notselected, selected[k]].ravel()
-		basis_eval[notselected, k] /= power_eval[selected[k]]
+			basis_eval[notselected, k] /= A[selected[k],selected[k]]
 
 		# computing the kth coefficient wrt the Newton basis
-		coeff[k, :] = residual_eval[selected[k], :] / power_eval[selected[k]]
+		if k > 0:
+			coeff[k, :] = residual_eval[k-1, selected[k], :] / power_eval[selected[k], k-1]
+		else:
+			coeff[k, :] = f[selected[k], :] / A[selected[k],selected[k]]
 
-		# updating the residuals
-		residual_eval = residual_eval - np.outer(basis_eval[:, k], coeff[k, :])
-		rmax[k] = np.max(np.sum(np.absolute(residual_eval[notselected,:]), axis=1))
-		v[k] = np.max(np.sum(np.absolute(residual_eval[selected,:]), axis=1))
+		# updating the residual
+		if k > 0:
+			residual_eval[k, :, :] = residual_eval[k-1, :, :] - np.outer(basis_eval[:, k], coeff[k, :])
+		else:
+			residual_eval[k, :, :] = f - np.outer(basis_eval[:, k], coeff[k, :])
 
 		# updating the basis transition matrix
 		change_of_basis[0:k, k] = - change_of_basis[0:k, 0:k] @ basis_eval[selected[k], 0:k].T
 		change_of_basis[k, k] = 1
-		change_of_basis[:, k] /= np.copy(power_eval[selected[k]])
+		if k > 0:
+			change_of_basis[:, k] /= np.copy(power_eval[selected[k], k-1]) # no need for copy?
+		else:
+			change_of_basis[:, k] /= A[selected[0], selected[0]]
 
 		#updating the power function
-		power_squared = power_eval[notselected]**2
+		if k > 0:
+			power_squared = power_eval[notselected, k-1]**2
+		else:
+			power_squared = A.diagonal()
 		basis_squared = basis_eval[notselected, k]**2
-		power_eval[notselected] = np.sqrt(np.abs(power_squared - basis_squared))
+		power_eval[notselected, k] = np.sqrt(np.abs(power_squared - basis_squared))
+		if k > 0:
+			power_eval[selected[0:k], k] = power_eval[selected[0:k], k-1]
 
 		if f_is_rkhs:
 			kernel_coeff = (change_of_basis[0:k+1, 0:k+1] @ coeff[0:k+1, :]).reshape((-1, q))
 			sum = 0
 			for i in range(q):
-				sum += np.inner(kernel_coeff[:, i], f[selected, i] - residual_eval[selected, i])
+				sum += np.inner(kernel_coeff[:, i], f[selected, i] - residual_eval[k, selected, i])
 			rkhs_error[k] = np.sqrt(np.absolute(norm_squarred - sum))
 
-		del notselected[i]
+		notselected.pop(selection_index)
 
 		# break if tolerance is met
-		if pmax[k] <= p_tolerance:
+		if np.max(power_eval[:,k]) <= p_tolerance:
 			num_iterations = k+1
 			# cutting of not needed space for more iterations
 			basis_eval = basis_eval[:, 0:num_iterations]
 			coeff = coeff[0:num_iterations, :]
+			residual_eval = residual_eval[0:num_iterations,:,:]
+			power_eval = power_eval[:, 0:num_iterations]
 			change_of_basis = change_of_basis[0:num_iterations, 0:num_iterations]
-			pmax = pmax[0:num_iterations]
-			rmax = rmax[0:num_iterations]
-			v = v[0:num_iterations]
 			if f_is_rkhs:
 				rkhs_error = rkhs_error[0:num_iterations]
 			break
@@ -120,6 +133,6 @@ def train(discrete_omega, kernel, f, max_iterations, p_tolerance, norm_squarred=
 	kernel_coeff = change_of_basis @ coeff
 
 	if f_is_rkhs:
-		return [selected, surrogate, kernel_coeff, residual_eval, power_eval, pmax, residual_quad,num_iterations, rkhs_error, rmax,v]
+		return [selected, surrogate, kernel_coeff, residual_eval, power_eval, residual_quad,num_iterations, rkhs_error]
 	else:
-		return [selected, surrogate, kernel_coeff, residual_eval, power_eval, pmax, residual_quad,num_iterations, rmax]
+		return [selected, surrogate, kernel_coeff, residual_eval, power_eval, residual_quad,num_iterations]
