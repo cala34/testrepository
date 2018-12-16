a433735443f607d3407343bdbacf48b92265bb0b
Celien <c_bosm01@uni-muenster.de>
2018-12-16 14:55:49 +0100

    final version

---
+++ applicationtest.py
@@ -0,0 +1,100 @@
+import numpy as np
+import math
+import kernels
+import pgreedy
+import matplotlib.pyplot as plt
+
+'''
+This script loads data from output.npy and performs kernel interpolation
+on a sample set of all feature vectors
+'''
+
+def print_accuracy(data_train, data_test, labels_train, labels_test, results):
+
+    num_iterations = results['num_iterations']
+
+    # function that computes the approximation value at x
+    def eval(x):
+        selected = results['selected']
+        kernel_coeff = results['kernel_coeff']
+        kernelvector = np.vectorize(lambda i: kernel(x, data_train[i, :]))
+        return kernelvector(selected) @ kernel_coeff
+
+    # array of approximation values for test data
+    test = np.zeros((len(data_test),10))
+    for i in range(len(data_test)):
+        test[i,:] = eval(data_test[i,:])
+
+    # compute the predicted class
+    surrogate = results['surrogate']
+    predictions_train = np.argmax(surrogate, axis=1)
+    predictions_test = np.argmax(test, axis=1)
+    # compute number of correct predictions / total number of predictions
+    accuracy_train = np.sum(predictions_train == labels_train) / len(predictions_train)
+    accuracy_test = np.sum(predictions_test == labels_test) / len(predictions_test)
+    print("train accuracy: ", accuracy_train)
+    print("test accuracy: ", accuracy_test)
+
+
+def plot_results(interpolation_data, results):
+
+    num_iterations = results['num_iterations']
+    pmax = results['max_power_fct']
+    rmean = results['mean_residual']
+    rmax = results['max_residual']
+
+    fig, ax = plt.subplots()
+    plt.xlabel('iteration')
+    ax.semilogy(range(num_iterations), pmax, '-', label = 'max power function')
+    ax.semilogy(range(num_iterations), rmax, '-.', label = 'max residual')
+    ax.semilogy(range(num_iterations), rmean, ':', label = 'mean residual')
+    legend = ax.legend(loc='center right')
+    for label in legend.get_lines():
+        label.set_linewidth(1.5)
+
+    plt.show()
+
+
+if __name__ == "__main__":
+
+    kernel = kernels.gausskernel(1/100)
+    num_data_train = 5000
+    num_data_test = 1000
+
+    # training parameters
+    train_param = {}
+    train_param['kernel'] = kernel
+    train_param['max_iterations'] = 1000
+    train_param['p_tolerance'] = math.pow(10, -7)
+    train_param['r_tolerance'] = math.pow(10, -7)
+
+    # load data and sample randomly
+    output = np.load("output.npy")
+    np.random.seed()
+    len_train = output[0].shape[0]
+    len_test = output[1].shape[0]
+    rand_train_indeces = np.random.randint(len_train, size = num_data_train)
+    rand_test_indeces = np.random.randint(len_test, size = num_data_test)
+    data_train = output[0][rand_train_indeces,:]
+    data_test = output[1][rand_test_indeces,:]
+    f_train = output[2][rand_train_indeces,:]
+    f_test = output[3][rand_test_indeces,:]
+    labels_train = output[4][rand_train_indeces]
+    labels_test = output[5][rand_test_indeces]
+
+    # interpolation data
+    interpolation_data = {}
+    interpolation_data['data'] = data_train
+    interpolation_data['f'] = f_train
+
+    # training
+    results = pgreedy.train(interpolation_data, train_param)
+
+    k = results['num_iterations'] - 1
+    residual = results['residual']
+    surrogate = results['surrogate']
+    f = interpolation_data['f']
+
+    print_accuracy(data_train, data_test, labels_train, labels_test, results)
+
+    plot_results(interpolation_data, results)
---
+++ plotsforexample.py
@@ -0,0 +1,146 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.axes as axes
+from mpl_toolkits.mplot3d import Axes3D
+import math
+
+import pgreedy
+import kernels
+
+'''
+script to produce plots in section 5.2 for Gaussian Kernel
+'''
+
+def grid(num_translates, kernel):
+    # set number of data sites
+    num_data = 2000
+    # initialize the random number generator
+    # np.random.seed() sets the seed to the current system time
+    # we chose a seed in order to reproduce the Thesis plots
+    np.random.seed(12)
+    # generate data from [0,1]^2 to interpolate on
+    data = 2 * np.random.rand(num_data, 2) - 1
+    # generate coefficients and centers for target function
+    centers = 2 * np.random.rand(num_translates, 2) - 1
+    alpha = 10 * np.random.rand(num_translates) - 5 # coefficients in [-5,5]
+
+    # compute values of target function
+    def model(x):
+        kernelvector = np.vectorize(lambda i: kernel(x, centers[i,:]))
+        K = kernelvector(range(num_translates))
+        return np.inner(alpha, K)
+    modelvectordata = np.vectorize(lambda i: model(data[i,:]))
+    f = modelvectordata(range(num_data))
+
+    # compute rkhs norm of target function
+    T = np.zeros((num_translates, num_translates))
+    for k in range(num_translates):
+        kernelcenters = np.vectorize(lambda i: kernel(centers[k,:], centers[i,:]))
+        T[k,:] = kernelcenters(range(num_translates))
+    rkhs_norm = alpha @ T @ alpha
+
+    # save interpolation data
+    interpolation_data = {'data': data, 'f': f.reshape((-1,1)), 'rkhs_norm_2': rkhs_norm}
+
+    # mesh grid for plotting the target function
+    X = np.arange(-1.5,1.5,.1)
+    Y = np.arange(-1.5,1.5,.1)
+    Z = np.zeros((len(X),len(X)))
+    for i in range(len(X)):
+        for j in range(len(X)):
+            Z[i,j] = model(np.array([X[i], Y[j]]))
+    X, Y = np.meshgrid(X, Y)
+    plot_f = {"X": X, "Y": Y, "Z": Z.T}
+
+    return interpolation_data, plot_f
+
+
+def plot_results(interpolation_data, results, plot_f, train_param):
+    # load variables
+    data = interpolation_data['data']
+    num_data = len(data)
+    rkhs_norm = interpolation_data['rkhs_norm_2']
+
+    kernel = train_param['kernel']
+
+    num_iterations = results['num_iterations']
+    selected = results['selected']
+    surrogate = results['surrogate']
+    kernel_coeff = results['kernel_coeff']
+    pmax = results['max_power_fct']
+    rmax = results['max_residual']
+    rkhs_error = results['rkhs_error']
+
+    X = plot_f['X']
+    Y = plot_f['Y']
+    Z = plot_f['Z']
+
+    fig, ax = plt.subplots()
+    ax.plot(data[:,0], data[:,1], 'b.', label = 'training data')
+    ax.plot(data[selected,0], data[selected,1], 'r.', label = 'selection')
+    legend = ax.legend(loc='upper right')
+    for label in legend.get_lines():
+        label.set_linewidth(1.5)
+    plt.title("Interpolation Data and Selection")
+
+    # plot max residual on data and its upper bound
+    fig, ax = plt.subplots()
+    plt.xlabel('iteration')
+    ax.semilogy(range(num_iterations), pmax*np.sqrt(rkhs_norm), '-', label = 'upper bound')
+    ax.semilogy(range(num_iterations), rmax, '-.', label = 'max residual')
+    legend = ax.legend(loc='upper right')
+    for label in legend.get_lines():
+        label.set_linewidth(1.5)
+    plt.title("Max Residual on Data and its Upper Bound (Power Function)")
+
+    fig, ax = plt.subplots()
+    plt.xlabel('iteration')
+    ax.semilogy(range(num_iterations), rkhs_error, '--', label = 'rkhs error')
+    legend = ax.legend(loc='upper right')
+    for label in legend.get_lines():
+        label.set_linewidth(1.5)
+    plt.title("RKHS Interpolation Error")
+
+    # plot target function
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+    ax.plot_surface(X, Y, Z, color='#052b68')
+    plt.title("Target Function")
+
+
+# Test
+if __name__ == "__main__":
+    # expansion size of target function
+    num_translates = 100
+    kernel = kernels.gausskernel()
+
+    # load data
+    interpolation_data, plot_f = grid(num_translates, kernel)
+
+    # training with tolerance
+
+    train_param = {}
+    train_param['kernel'] = kernel
+    train_param['max_iterations'] = 190
+    train_param['p_tolerance'] = math.pow(10, -13)
+    train_param['r_tolerance'] = math.pow(10, -13)
+
+    results = pgreedy.train(interpolation_data, train_param)
+
+    plot_results(interpolation_data, results, plot_f, train_param)
+
+    # training until max expansion size
+
+    train_param = {}
+    train_param['kernel'] = kernel
+    train_param['max_iterations'] = 190
+
+    results = pgreedy.train(interpolation_data, train_param)
+
+    print("min residual of", np.min(results['max_residual']), "at iteration", np.argmin(results['max_residual']))
+    print("min power of", np.min(results['max_power_fct']), "at iteration", np.argmin(results['max_power_fct']))
+    print("min rkhs error of", np.min(results['rkhs_error']), "at iteration", np.argmin(results['rkhs_error']))
+
+    plot_results(interpolation_data, results, plot_f, train_param)
+
+    plt.show()
